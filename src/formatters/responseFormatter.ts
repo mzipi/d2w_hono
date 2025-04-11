319a7ba95b785fe87904a26565ad25ce4e563939
Michell Zipitría <mzipi@outlook.com>
2025-04-11 05:27:26 -0300

    feat: Enhance response formatting by including breaker type for enriched weapons

--- src/formatters/responseFormatter.ts
+++ src/formatters/responseFormatter.ts
@@ -1,3 +1,5 @@
+import { filterStatGroupHashes, filterItemPresentation, findPlugSetsByWeapon, findPlugItemByPlugSet } from "../helpers/helpers.ts"
+
 export default function formatResponse(data) {
     const {
         weaponsFound,
@@ -21,6 +23,7 @@ export default function formatResponse(data) {
         const collectible = collectiblesFound[weapon.collectibleHash];
         const damageType = damageTypesFound[weapon.defaultDamageTypeHash];
         const equipmentSlot = equipmentSlotFound[weapon.equippingBlock.equipmentSlotTypeHash];
+        const breakerType = breakerTypeFound[weapon.breakerTypeHash];
 
         const categories = [];
         for (const categoryHash of weapon.itemCategoryHashes || []) {
@@ -35,6 +38,7 @@ export default function formatResponse(data) {
             collectible,
             damageType,
             equipmentSlot,
+            breakerType,
             categories
         };
     }
